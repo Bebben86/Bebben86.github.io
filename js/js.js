6d875b2b440a7ad82ed12b2d94e8c454371f3a9d
Andreas Nilsson <andreas.j.nilsson@outlook.com>
2021-10-04 11:39:15 +0200

    Homepage 2.0. New design alltogether.

--- js/js.js
+++ js/js.js
@@ -1,14 +1,27 @@
-//Fade in boxes
+//Fade in elements
 $(document).ready(function () {
-  $(".box").hide().fadeIn(1000);
+  $(".headline").hide().fadeIn(1000);
 });
 
 $(document).ready(function () {
-  $(".box2").hide().fadeIn(1500);
+  $(".reddot").hide().fadeIn(1000);
 });
 
-//Toggle dark mode
-function themeToggler() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
+$(document).ready(function () {
+  $(".avatar").hide().fadeIn(1500);
+});
+
+$(document).ready(function () {
+  $(".main").hide().fadeIn(1500);
+});
+$(document).ready(function () {
+  $(".link_table").hide().fadeIn(2500);
+});
+
+//Toggle language
+$('[lang="en"]').hide();
+
+$('#switch-lang').click(function() {
+  $('[lang="sv"]').toggle();
+  $('[lang="en"]').toggle();
+});
